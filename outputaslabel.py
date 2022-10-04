9d4aabd798f1ccf234e766fcaf7eb2f4b158baaf
Dalton <DY@test.com>
2022-10-04 12:58:52 -0500

    added output as label file. printing w/o py popup.

---
+++ outputaslabel.py
@@ -0,0 +1,61 @@
+import tkinter
+import tkinter.messagebox
+
+class myGUI:
+
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+
+        self.main_window.geometry('500x200')
+        self.main_window.title('Label Demo')
+
+        self.topframe = tkinter.Frame(self.main_window)
+        self.midframe = tkinter.Frame(self.main_window)
+        self.bottomframe = tkinter.Frame(self.main_window)
+
+        self.prompt_label = tkinter.Label(self.topframe, text = "Enter a Distance in kilometers")
+        self.entry = tkinter.Entry(self.topframe, width=10)
+        
+
+        self.prompt_label.pack(side='left')
+        self.entry.pack(side='right')
+
+
+        self.miles_var = tkinter.StringVar()
+
+        
+        self.desc_label = tkinter.Label(self.midframe, text = "Converted to Miles")
+            #Hardcoding the text
+        self.miles = tkinter.Label(self.midframe,textvariable=self.miles_var)
+            #Not hard coding the text, rather uses a variable 'self.miles_var'
+
+
+        self.desc_label.pack(side = 'left')
+        self.miles.pack(side = 'left')
+
+
+        self.topframe.pack(side = 'top')
+        self.midframe.pack()
+        self.bottomframe.pack(side = 'top')
+
+        self.mybutton = tkinter.Button(self.main_window, text='Convert', command=self.convert)
+        self.quitbutton = tkinter.Button(self.main_window, text='Quit', command=self.main_window.destroy)
+        
+        self.mybutton.pack(side='left')
+        self.quitbutton.pack(side='right')
+
+        tkinter.mainloop()
+
+    def convert(self):
+        kilo = float(self.entry.get())
+
+        miles = round(kilo * 0.6214,2)
+
+        self.miles_var.set(miles)
+            #The set method allows us to put information into a variable we have created, in this instance 'miles_var'
+
+        #tkinter.messagebox.showinfo('Result', str(kilo) +'kilometers is equal to' + str(miles) + ' miles')
+
+my_gui = myGUI()
+
+print("I am done.")
